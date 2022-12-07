2f605f829a9e90d8c3f1bff951e999b5e0d1edfb
Nic McPhee <mcphee@morris.umn.edu>
2022-12-06 20:22:38 -0600

    Solve Day 01 in Rust
    This was pretty straightforward, although the argument to `sum_group` has an ugly type and can presumably be replaced with some sort of simpler iterator.

---
+++ rust-advent/src/bin/day_01.rs
@@ -0,0 +1,26 @@
+use std::fs;
+use std::str::SplitAsciiWhitespace;
+
+fn main() {
+    let contents = fs::read_to_string("../inputs/day_01.input")
+        .expect("Should have been able to read the file");
+
+    let mut totals: Vec<usize> = contents
+        .split("\n\n")
+        .map(|g| g.split_ascii_whitespace())
+        .map(|g| sum_group(g))
+        .collect();
+
+    let biggest = totals.iter().max().unwrap();
+
+    println!("The largest sum was {biggest}");
+
+    totals.sort();
+    let biggest_three: usize = totals.iter().rev().take(3).sum();
+
+    println!("The sum of the three largest values was {biggest_three}.");
+}
+
+fn sum_group(group: SplitAsciiWhitespace) -> usize {
+    group.map(|s| s.parse::<usize>().unwrap()).sum()
+}
