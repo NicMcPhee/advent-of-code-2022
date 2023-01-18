ef92c831b656729d64d65ed996069188a43c8ba9
Nic McPhee <mcphee@morris.umn.edu>
2023-01-18 17:09:18 -0600

    Reset to starting position
    This removes all the code for Days 1-5, and leaves us with a stub for Day 1.

--- rust-advent/src/bin/day_01.rs
+++ rust-advent/src/bin/day_01.rs
@@ -3,41 +3,8 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use anyhow::{anyhow, Context, Result};
-use std::fs;
-use std::str::SplitAsciiWhitespace;
+use anyhow::Result;
 
 fn main() -> Result<()> {
-    let input_file = "../inputs/day_01.input";
-    let contents = fs::read_to_string(input_file)
-        .with_context(|| format!("Failed to open the input file '{input_file}'"))?;
-
-    let mut totals: Vec<usize> = contents
-        .split("\n\n")
-        .map(str::split_ascii_whitespace)
-        .map(sum_group)
-        .collect::<Result<Vec<_>, _>>()?;
-
-    let biggest = totals
-        .iter()
-        .max()
-        .ok_or_else(|| anyhow!("Empty list of totals"))?;
-
-    println!("The largest sum was {biggest}");
-
-    totals.sort_unstable();
-    let biggest_three: usize = totals.iter().rev().take(3).sum();
-
-    println!("The sum of the three largest values was {biggest_three}.");
-
-    Ok(())
-}
-
-fn sum_group(group: SplitAsciiWhitespace) -> Result<usize> {
-    group
-        .map(|s| {
-            s.parse::<usize>()
-                .with_context(|| format!("Failed to parse '{}' to `usize`.", s))
-        })
-        .sum::<Result<usize>>()
+    todo!()
 }
