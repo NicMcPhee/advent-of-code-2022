373c06396f9af6f3883c7e48d3d0f2a7284f6a9a
Nic McPhee <mcphee@morris.umn.edu>
2023-02-04 15:05:49 -0600

    Complete Day 06, part 1!
    Not too complicated. We learned some cool things about hashing references in the implementation of `all_unique()` thanks to @esitsu@Twitch.

--- rust-advent/src/bin/day_06_part_1.rs
+++ rust-advent/src/bin/day_06_part_1.rs
@@ -3,17 +3,60 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use std::{fs, str::FromStr};
+use std::{collections::HashSet, fs};
 
-use anyhow::{ensure, Context, Result};
+use anyhow::{Context, Result};
 
-static INPUT_FILE: &str = "../inputs/day_05.input";
+static INPUT_FILE: &str = "../inputs/day_06.input";
+
+// TODO: Try using Rayon's `par_windows()` and do some benchmarking
+//  to see how much faster that makes things.
+fn find_marker(s: &str) -> Result<usize> {
+    let (pos, _) = s
+        .as_bytes()
+        .windows(4)
+        .enumerate()
+        .find(|(_, cs)| all_unique(cs))
+        .context("There were no unique windows in the input")?;
+
+    // The marker is four characters ahead of the start of the first block
+    // of unique characters.
+    Ok(pos + 4)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn third_example() -> Result<()> {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let result = find_marker(input)?;
+        assert_eq!(10, result);
+        Ok(())
+    }
+}
+
+fn all_unique(cs: &[u8]) -> bool {
+    // This works because hashing on &T is converting to hashing
+    // on T, so we end up hashing on the underlying `u8`s (and
+    // not their addresses as I had thought we would), which
+    // makes everything work.
+    // See https://doc.rust-lang.org/std/hash/trait.Hash.html#impl-Hash-for-%26T
+    // and click on the `source` to see what actually happens.
+    // Thanks to @esitsu for pointing this out, in response to a
+    // question from @ikopor.
+    let unique_chars: HashSet<&u8> = cs.iter().collect();
+    cs.len() == unique_chars.len()
+}
 
 fn main() -> Result<()> {
     let contents = fs::read_to_string(INPUT_FILE)
         .with_context(|| format!("Failed to open file '{INPUT_FILE}'"))?;
 
-    todo!();
+    let marker = find_marker(&contents)?;
+
+    println!("The marker was {marker}");
 
     Ok(())
 }
