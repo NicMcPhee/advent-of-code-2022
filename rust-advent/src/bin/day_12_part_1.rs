5630fc3dab19d9f4bb4863b66f20e4b7529a6fd5
Nic McPhee <mcphee@morris.umn.edu>
2023-03-11 15:26:45 -0600

    Parse Terrain for Day 12
    This parses the Terrain.

--- rust-advent/src/bin/day_12_part_1.rs
+++ rust-advent/src/bin/day_12_part_1.rs
@@ -11,17 +11,63 @@ use std::{
 use anyhow::{Context, Result};
 
 #[derive(Debug)]
+enum Height {
+    Start,
+    End,
+    Height(u8),
+}
+
+impl Height {
+    fn new(c: char) -> Self {
+        match c {
+            'S' => Self::Start, // Start has height 0
+            'E' => Self::End,   // End has height 26
+            _ => Self::Height(c as u8 - 'a' as u8),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Location {
+    x: usize,
+    y: usize,
+    dist: u32,
+}
+
+#[derive(Debug)]
 struct Terrain {
-    heights: Vec<Vec<char>>,
+    heights: Vec<Vec<Height>>,
+    start: Location,
+    end: Location,
 }
 
 impl FromStr for Terrain {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        todo!()
+        let mut start: Option<Location> = None;
+        let mut end: Option<Location> = None;
+        let mut heights: Vec<Vec<Height>> = Vec::new();
+        for (x, line) in s.lines().enumerate() {
+            let mut row_heights: Vec<Height> = Vec::with_capacity(line.len());
+            for (y, c) in line.chars().enumerate() {
+                let height = Height::new(c);
+                match height {
+                    Height::Start => start = Some(Location { x, y, dist: u32::MAX }),
+                    Height::End => end = Some(Location { x, y, dist: u32::MAX }),
+                    _ => { /* Do nothing */}
+                };
+                row_heights.push(height);
+            }
+            heights.push(row_heights);
+        }
+        let start = start.context("We never found the start location")?;
+        let end = end.context("We never found the end location")?;
+        Ok(Terrain { heights, start, end })
     }
 }
+
+impl Terrain {}
 
 static INPUT_FILE: &str = "../inputs/day_12_test.input";
 
@@ -29,6 +75,8 @@ fn main() -> Result<()> {
     let terrain = fs::read_to_string(INPUT_FILE)
         .with_context(|| format!("Failed to open file '{INPUT_FILE}'"))?
         .parse::<Terrain>()?;
+
+    println!("{terrain:?}");
 
     todo!();
 
