c157745be1b9ef2ef3a17deb0871306840f18281
Nic McPhee <mcphee@morris.umn.edu>
2023-04-08 13:48:47 -0500

    Add esitsu@Twitch & ikopor@Twitch's versions of the `PartialOrd`
    Both esitsu and ikopor suggested variants on Discord of my `PartialOrd` implementation that don't generate new `Vec`s. This pastes their versions in so we can talk about them and do some timing.

--- rust-advent/src/bin/day_13_part_1.rs
+++ rust-advent/src/bin/day_13_part_1.rs
@@ -7,6 +7,7 @@ use nom::combinator::map;
 use nom::multi::separated_list0;
 use nom::sequence::separated_pair;
 use nom::{sequence::delimited, IResult};
+use std::cmp::Ordering;
 use std::fs;
 
 use anyhow::{Context, Result};
@@ -22,14 +23,80 @@ struct PacketPair {
     right: Packet,
 }
 
+
+// // Original version
+// impl PartialOrd for Packet {
+//     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+//         match (self, other) {
+//             (Self::Value(l), Self::Value(r)) => l.partial_cmp(r),
+//             (Self::List(ls), Self::List(rs)) => ls.partial_cmp(rs),
+//             (Self::Value(l), Self::List(rs)) => vec![Self::Value(*l)].partial_cmp(rs),
+//             (Self::List(ls), Self::Value(r)) => ls.partial_cmp(&vec![Self::Value(*r)]),
+//         }
+//     }
+// }
+
+// // ikopor@Twitch's version
+// impl PartialOrd for Packet {
+//     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+//         match (self, other) {
+//             (Self::Value(l), Self::Value(r)) => l.partial_cmp(r),
+//             (Self::List(ls), Self::List(rs)) => ls.partial_cmp(rs),
+//             (Self::Value(l), Self::List(r)) => {
+//                 let l: &[Packet] = &[Self::Value(*l)];
+//                 l.partial_cmp(r)
+//             }
+//             (Self::List(_), Self::Value(_)) => other.partial_cmp(self).map(Ordering::reverse),
+//         }
+//     }
+// }
+
+// esitsu@Twitch's version
 impl PartialOrd for Packet {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match (self, other) {
             (Self::Value(l), Self::Value(r)) => l.partial_cmp(r),
-            (Self::List(ls), Self::List(rs)) => ls.partial_cmp(rs),
-            (Self::Value(l), Self::List(rs)) => vec![Self::Value(*l)].partial_cmp(rs),
-            (Self::List(ls), Self::Value(r)) => ls.partial_cmp(&vec![Self::Value(*r)]),
+            (Self::List(l), Self::List(r)) => l.partial_cmp(r),
+            (Self::Value(l), r @ Self::List(_)) => l.partial_cmp(r),
+            (l @ Self::List(_), Self::Value(r)) => l.partial_cmp(r),
         }
+    }
+}
+
+impl PartialEq<Packet> for u8 {
+    fn eq(&self, other: &Packet) -> bool {
+        match other {
+            Packet::Value(val) => self == val,
+            Packet::List(_) => false,
+        }
+    }
+}
+
+impl PartialOrd<Packet> for u8 {
+    fn partial_cmp(&self, other: &Packet) -> Option<Ordering> {
+        match other {
+            Packet::Value(val) => self.partial_cmp(val),
+            Packet::List(list) => match &list[..] {
+                [] => Some(Ordering::Greater),
+                [item] => self.partial_cmp(item),
+                [item, ..] => match self.partial_cmp(item) {
+                    Some(Ordering::Equal) => Some(Ordering::Less),
+                    ord => ord,
+                }
+            }
+        }
+    }
+}
+
+impl PartialEq<u8> for Packet {
+    fn eq(&self, other: &u8) -> bool {
+        other == self
+    }
+}
+
+impl PartialOrd<u8> for Packet {
+    fn partial_cmp(&self, other: &u8) -> Option<Ordering> {
+        other.partial_cmp(self).map(Ordering::reverse)
     }
 }
 
@@ -85,6 +152,15 @@ fn packet(s: &str) -> IResult<&str, Packet> {
     map(delimited(char('['), element_list, char(']')), Packet::List)(s)
 }
 
+fn compute_sum(packet_pairs: Vec<PacketPair>) -> usize {
+    packet_pairs
+        .iter()
+        .enumerate()
+        .filter(|(_, packet_pair)| packet_pair.is_ordered()) // Give us just the correctly ordered pairs
+        .map(|(i, _)| i + 1) // Give us the indices+1 of correctly ordered pairs
+        .sum::<usize>()
+}
+
 static INPUT_FILE: &str = "../inputs/day_13.input";
 
 fn main() -> Result<()> {
@@ -93,12 +169,7 @@ fn main() -> Result<()> {
 
     let (_, packet_pairs) = packet_pair_list(&contents).map_err(|e| e.to_owned())?;
 
-    let result = packet_pairs
-        .iter()
-        .enumerate()
-        .filter(|(_, packet_pair)| packet_pair.is_ordered()) // Give us just the correctly ordered pairs
-        .map(|(i, _)| i+1) // Give us the indices+1 of correctly ordered pairs
-        .sum::<usize>();
+    let result = compute_sum(packet_pairs);
 
     println!("The final sum was {result}.");
 
