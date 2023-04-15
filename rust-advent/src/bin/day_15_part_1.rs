67b3b57e4ee9742b1dbfef15519a4e38255c1e1f
Nic McPhee <mcphee@morris.umn.edu>
2023-04-15 16:08:58 -0500

    Solve Part 1 of Day 15
    I did some math on the (which really ought to be in a comment on `SensorBeacon::row_range`) where we can, for each sensor, compute a range that represents the intersection of that sensors coverage area and the target row.
    
    The `range_union_find` crate was nice for creating a union of those ranges. It also let us compute the total coverage of the range union, which is nearly the right answer.
    
    The last bit was to find all the beacons on that row, and subtract them from the count since we're looking for places where there can not be a beacon (and those represent places there definitely is a beacon).
    
    It turns out that its possible (and apparently common) for multiple sensors to detect and report the same beacon, so we used the `unique()` function from `itertools` to get a unique set of beacon points before filtering and counting.

--- rust-advent/src/bin/day_15_part_1.rs
+++ rust-advent/src/bin/day_15_part_1.rs
@@ -3,12 +3,14 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use std::fs;
+use std::{fs, ops::RangeInclusive};
 
 use anyhow::Context;
+use itertools::Itertools;
+use range_union_find::IntRangeUnionFind;
 use regex::{Captures, Regex};
 
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
@@ -19,10 +21,49 @@ struct Sensor(Point);
 #[derive(Debug)]
 struct Beacon(Point);
 
+#[derive(Debug)]
+struct SensorBeacon {
+    sensor: Sensor,
+    beacon: Beacon,
+    manhattan_distance: u32,
+}
+
+impl SensorBeacon {
+    const fn new(sensor: Sensor, beacon: Beacon) -> Self {
+        Self {
+            manhattan_distance: Self::md(sensor.0, beacon.0),
+            sensor,
+            beacon,
+        }
+    }
+
+    // TODO: Add a comment that explains the math here. Probably want to
+    //   change some of the variables at the same time so the whole thing
+    //   makes a little more sense.
+    #[allow(clippy::cast_possible_wrap)]
+    fn row_range(&self, row: i32) -> RangeInclusive<i32> {
+        let sensor_row_dist = self.sensor.0.y.abs_diff(row);
+        if sensor_row_dist > self.manhattan_distance {
+            // If the sensor_row_dist is larger than the Manhattan distance
+            // then the row is entirely outside the coverage area for the
+            // sensor, and we want to return an empty range.
+            #[allow(clippy::reversed_empty_ranges)]
+            return 1..=0;
+        }
+        let ub: i32 = self.manhattan_distance as i32 - sensor_row_dist as i32;
+        let lhs = self.sensor.0.x - ub;
+        let rhs = self.sensor.0.x + ub;
+        lhs.min(rhs)..=lhs.max(rhs)
+    }
+
+    const fn md(sensor: Point, beacon: Point) -> u32 {
+        sensor.x.abs_diff(beacon.x) + sensor.y.abs_diff(beacon.y)
+    }
+}
+
 #[derive(Default)]
 struct Cave {
-    sensors: Vec<Sensor>,
-    beacons: Vec<Beacon>,
+    sensor_beacons: Vec<SensorBeacon>,
 }
 
 impl Cave {
@@ -35,15 +76,61 @@ impl Cave {
             x: capture[3].parse()?,
             y: capture[4].parse()?,
         });
+        let sensor_beacon = SensorBeacon::new(sensor, beacon);
 
-        self.sensors.push(sensor);
-        self.beacons.push(beacon);
+        self.sensor_beacons.push(sensor_beacon);
 
         Ok(())
     }
+
+    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
+    fn coverage(&self, row: i32) -> anyhow::Result<u32> {
+        // Replace the `for` loop with a fold or reduce?
+        let mut union_range = IntRangeUnionFind::new();
+        for r in self.row_ranges(row) {
+            if !r.is_empty() {
+                union_range.insert_range(&r)
+                    .with_context(|| format!("Adding {r:?} to {union_range:?} failed"))?;
+            }
+        }
+        println!("{union_range:?}");
+
+        // The subtraction just before `.sum()` can never return a negative
+        // value because the `end` of a range can't be less than the `start`
+        // a range. (Maybe this is only partly true, but we'll hope that
+        // no one is putting weird ranges into the system.)
+        #[allow(clippy::cast_sign_loss)]
+        let initial_count: u32 = union_range
+            .to_collection::<Vec<_>>()
+            .iter()
+            .map(|r| (r.end() - r.start() + 1) as u32)
+            .sum();
+
+        println!("The initial count is {initial_count}.");
+
+        let num_beacons_in_row = self
+            .sensor_beacons
+            .iter()
+            .map(|sb| sb.beacon.0)
+            .unique()
+            .map(|pt| pt.y)
+            .filter(|y| *y == row && union_range.has_element(y))
+            .count() as u32;
+
+        println!("The number of beacons in the range was {num_beacons_in_row}.");
+
+        Ok(initial_count - num_beacons_in_row)
+    }
+
+    fn row_ranges(&self, row: i32) -> Vec<RangeInclusive<i32>> {
+        self.sensor_beacons
+            .iter()
+            .map(|sensor_beacon| sensor_beacon.row_range(row))
+            .collect()
+    }
 }
 
-static INPUT_FILE: &str = "../inputs/day_15_test.input";
+static INPUT_FILE: &str = "../inputs/day_15.input";
 
 fn main() -> anyhow::Result<()> {
     let mut cave = Cave::default();
@@ -59,8 +146,13 @@ fn main() -> anyhow::Result<()> {
         cave.add_entry(&cap)?;
     }
 
-    println!("Sensors: {:?}", cave.sensors);
-    println!("Beacons: {:?}", cave.beacons);
+    // println!("Sensors: {:?}", cave.sensor_beacons);
+
+    println!("The row ranges are {:?}", cave.row_ranges(10));
+
+    let num_covered = cave.coverage(2_000_000)?;
+
+    println!("The number of covered locations = {num_covered}");
 
     Ok(())
 }
