78b75ed5548c0f19e8b09d2a3399d05817b826fc
Nic McPhee <mcphee@morris.umn.edu>
2023-04-26 22:15:20 -0500

    Implement the parsing for Day 16
    This uses `nom` to implement the parsing for Day 16. There's a lot I like about `nom`, but importing the right thing continues to be a challenge, and the whole `IResult` thing is still kinda confusing when converting from that to `anyhow::Result`. We ended up with the `extract_valve`/`valve` combo, which seemed a bit awkward. I wonder if I want a generic helper function that converts `IResult`s into `anyhow::Result`?
    
    With this we're able to parse out all the `Valve` structs. We now need to construct the graph datatype that this represents. Actually solving the problem will involve some complex algorithms fun (maybe A*?), so I'll need to do a little homework before the next session so I'm ready to implement that.

---
+++ rust-advent/src/bin/day_16_part_1.rs
@@ -0,0 +1,67 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::expect_used)]
+
+use std::fs;
+
+use anyhow::Context;
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::u32,
+    combinator::{all_consuming, map},
+    multi::separated_list1,
+    sequence::preceded,
+    IResult,
+};
+
+#[derive(Debug)]
+struct Valve {
+    name: String,
+    flow_rate: u32,
+    adjacent_valve_names: Vec<String>,
+}
+
+// Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+fn valve(line: &str) -> IResult<&str, Valve> {
+    let (s, name) = preceded(tag("Valve "), take(2usize))(line)?;
+    let (s, flow_rate) = preceded(tag(" has flow rate="), u32)(s)?;
+    let (s, adjacent_valve_names) = all_consuming(preceded(
+        alt((
+            tag("; tunnel leads to valve "),
+            tag("; tunnels lead to valves "),
+        )),
+        separated_list1(tag(", "), map(take(2usize), std::string::ToString::to_string)),
+    ))(s)?;
+
+    Ok((
+        s,
+        Valve {
+            name: name.to_string(),
+            flow_rate,
+            adjacent_valve_names,
+        },
+    ))
+}
+
+fn extract_valve(line: &str) -> anyhow::Result<Valve> {
+    // This version might actually be easier to understand, although Clippy
+    // reasonably prefers not adding a closure unnecessarily.
+    //     Ok(valve(line).map_err(|e| e.to_owned())?.1)
+    Ok(valve(line).map_err(nom::Err::<nom::error::Error<&str>>::to_owned)?.1)
+}
+
+static INPUT_FILE: &str = "../inputs/day_16_test.input";
+
+fn main() -> anyhow::Result<()> {
+    let valves = fs::read_to_string(INPUT_FILE)
+        .with_context(|| format!("Failed to open file '{INPUT_FILE}'"))?
+        .lines()
+        .map(extract_valve)
+        .collect::<anyhow::Result<Vec<Valve>>>()?;
+
+    println!("{valves:?}");
+
+    Ok(())
+}
