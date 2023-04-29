dc75d9dc384e793d4264ab40af9a5ed7f7f914b1
Nic McPhee <mcphee@morris.umn.edu>
2023-04-29 16:54:51 -0500

    Add a `BitSet` type, with a few tests
    MizardX@Twitch had suggested using a `u64` bit set instead of a `HashSet` like I was using since the bit set would be a lot easier to clone (and we'll need to clone it a lot).
    
    I explored using https://github.com/contain-rs/bit-set, but it seems wired to only use `u32` for its bits, and we need `u64`. :-(
    
    So I ended up making my own. I really didn't trust myself on that, so I actually added some tests to make sure that it's doing the right thing.

--- rust-advent/src/bin/day_16_part_1.rs
+++ rust-advent/src/bin/day_16_part_1.rs
@@ -50,6 +50,62 @@ fn extract_valve(line: &str) -> anyhow::Result<Valve> {
     // reasonably prefers not adding a closure unnecessarily.
     //     Ok(valve(line).map_err(|e| e.to_owned())?.1)
     Ok(valve(line).map_err(nom::Err::<nom::error::Error<&str>>::to_owned)?.1)
+struct BitSet {
+    bits: u64,
+}
+
+impl BitSet {
+    #[must_use]
+    fn add(&self, position: u16) -> Self {
+        assert!(position < 64);
+        let bit = 1u64 << position;
+        Self {
+            bits: self.bits | bit,
+        }
+    }
+
+    #[must_use]
+    fn contains(&self, position: u16) -> bool {
+        assert!(position < 64);
+        let bit = 1u64 << position;
+        self.bits & bit > 0
+    }
+}
+
+#[cfg(test)]
+mod bit_set_tests {
+    use super::*;
+
+    #[test]
+    fn empty_bit_set() {
+        let empty = BitSet { bits: 0 };
+        for i in 0..64 {
+            assert!(empty.contains(i).not());
+        }
+    }
+
+    #[test]
+    fn full_bit_set() {
+        let mut bits = BitSet { bits: 0 };
+        for i in 0..64 {
+            bits = bits.add(i);
+        }
+        for i in 0..64 {
+            assert!(bits.contains(i));
+        }
+    }
+
+    #[test]
+    fn only_even_bits() {
+        let mut bits = BitSet { bits: 0 };
+        for i in 0..32 {
+            bits = bits.add(i * 2);
+        }
+        for i in 0..64 {
+            assert_eq!(bits.contains(i), i % 2 == 0);
+        }
+    }
+}
 }
 
 static INPUT_FILE: &str = "../inputs/day_16_test.input";
