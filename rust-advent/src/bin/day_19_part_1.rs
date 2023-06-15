bf8bb9ae592ea8bef91bb3a1dad7b82acbafdc85
Nic McPhee <mcphee@morris.umn.edu>
2023-06-14 22:01:12 -0500

    Implement basic framework for Part 1, Day 19
    This gets the basic structure for Day 19 (Part 1) written, but the memoization totally failed to do what I wanted (& we needed) and things will run forever in the current setup.
    
    I'm obviously going to have to work out something fancier for the dynamic programming/memoization to make this run in a reasonable amount of time.

--- rust-advent/src/bin/day_19_part_1.rs
+++ rust-advent/src/bin/day_19_part_1.rs
@@ -3,22 +3,28 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
+use memoize::memoize;
 use once_cell::sync::Lazy;
 use regex::Regex;
-use std::{fs, marker::PhantomData, str::FromStr};
+use std::{
+    fs,
+    marker::PhantomData,
+    ops::{Add, Sub},
+    str::FromStr,
+};
 
 use anyhow::Context;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Ore;
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Clay;
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Obsidian;
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Geode;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Robot<T> {
     ore_cost: u8,
     clay_cost: u8,
@@ -70,7 +76,7 @@ impl Robot<Geode> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Blueprint {
     number: u8,
     ore: Robot<Ore>,
@@ -81,14 +87,6 @@ struct Blueprint {
 
 // Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 // Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
-
-/*
-let re = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
-let text = "2012-03-14, 2013-01-01 and 2014-07-05";
-for cap in re.captures_iter(text) {
-    println!("Month: {} Day: {} Year: {}", &cap[2], &cap[3], &cap[1]);
-}
- */
 
 #[allow(clippy::expect_used)]
 static IS_NUMBER: Lazy<Regex> =
@@ -112,7 +110,224 @@ impl FromStr for Blueprint {
     }
 }
 
-static INPUT_FILE: &str = "../inputs/day_19.input";
+#[derive(Debug, Eq, PartialEq, Hash, Default, Copy, Clone)]
+struct Resources {
+    num_ore: u8,
+    num_clay: u8,
+    num_obsidian: u8,
+    num_geodes: u8,
+}
+
+impl Resources {
+    const fn from_robot<T>(robot: &Robot<T>) -> Self {
+        Self {
+            num_ore: robot.ore_cost,
+            num_clay: robot.clay_cost,
+            num_obsidian: robot.obsidian_cost,
+            num_geodes: 0,
+        }
+    }
+}
+
+impl Add for Resources {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        #[allow(clippy::expect_used)]
+        Self {
+            num_ore: self
+                .num_ore
+                .checked_add(rhs.num_ore)
+                .expect("We overflowed the number of ore"),
+            num_clay: self
+                .num_clay
+                .checked_add(rhs.num_clay)
+                .expect("We overflowed the number of clay"),
+            num_obsidian: self
+                .num_obsidian
+                .checked_add(rhs.num_obsidian)
+                .expect("We overflowed the number of obsidian"),
+            num_geodes: self
+                .num_geodes
+                .checked_add(rhs.num_geodes)
+                .expect("We overflowed the number of geodes"),
+        }
+    }
+}
+
+impl Sub for Resources {
+    type Output = Option<Self>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Some(Self {
+            num_ore: self.num_ore.checked_sub(rhs.num_ore)?,
+            num_clay: self.num_clay.checked_sub(rhs.num_clay)?,
+            num_obsidian: self.num_obsidian.checked_sub(rhs.num_obsidian)?,
+            num_geodes: self.num_geodes.checked_sub(rhs.num_geodes)?,
+        })
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+struct RobotCounts {
+    num_ore_robots: u8,
+    num_clay_robots: u8,
+    num_obsidian_robots: u8,
+    num_geode_robots: u8,
+}
+
+impl RobotCounts {
+    const fn add_ore_robot(self) -> Self {
+        Self {
+            num_ore_robots: self.num_ore_robots + 1,
+            ..self
+        }
+    }
+
+    const fn add_clay_robot(self) -> Self {
+        Self {
+            num_clay_robots: self.num_clay_robots + 1,
+            ..self
+        }
+    }
+
+    const fn add_obsidian_robot(self) -> Self {
+        Self {
+            num_obsidian_robots: self.num_obsidian_robots + 1,
+            ..self
+        }
+    }
+
+    const fn add_geode_robot(self) -> Self {
+        Self {
+            num_geode_robots: self.num_geode_robots + 1,
+            ..self
+        }
+    }
+}
+
+impl Default for RobotCounts {
+    fn default() -> Self {
+        Self {
+            num_ore_robots: 1,
+            num_clay_robots: 0,
+            num_obsidian_robots: 0,
+            num_geode_robots: 0,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct State {
+    remaining_minutes: u8,
+    resources: Resources,
+    robot_counts: RobotCounts,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            remaining_minutes: 24,
+            resources: Resources::default(),
+            robot_counts: RobotCounts::default(),
+        }
+    }
+}
+
+impl State {
+    const fn new_resources(&self) -> Resources {
+        Resources {
+            num_ore: self.robot_counts.num_ore_robots,
+            num_clay: self.robot_counts.num_clay_robots,
+            num_obsidian: self.robot_counts.num_obsidian_robots,
+            num_geodes: self.robot_counts.num_geode_robots,
+        }
+    }
+}
+
+#[memoize]
+fn max_geodes(blueprint: Blueprint, state: State) -> u8 {
+    if state.remaining_minutes == 0 {
+        return state.resources.num_geodes;
+    }
+    println!("{state:?}");
+    #[allow(clippy::expect_used)]
+    blueprint
+        .child_states(&state)
+        .into_iter()
+        .map(|s| max_geodes(blueprint.clone(), s))
+        .max()
+        .expect("There should have been at least one child state")
+}
+
+impl Blueprint {
+    fn quality_level(&self) -> usize {
+        let state = State::default();
+        usize::from(self.number) * usize::from(max_geodes(self.clone(), state))
+    }
+
+    fn child_states(&self, state: &State) -> Vec<State> {
+        // Make robots & collect resources
+        [
+            Self::make_no_robot,
+            Self::make_ore_robot,
+            Self::make_clay_robot,
+            Self::make_obsidian_robot,
+            Self::make_geode_robot,
+        ]
+        .into_iter()
+        .filter_map(|f| f(self, state))
+        .collect::<Vec<_>>()
+    }
+
+    #[allow(clippy::unused_self, clippy::unnecessary_wraps)]
+    fn make_no_robot(&self, state: &State) -> Option<State> {
+        Some(State {
+            remaining_minutes: state.remaining_minutes - 1,
+            resources: state.resources + state.new_resources(),
+            robot_counts: state.robot_counts,
+        })
+    }
+
+    fn make_ore_robot(&self, state: &State) -> Option<State> {
+        Some(State {
+            remaining_minutes: state.remaining_minutes - 1,
+            resources: (state.resources - Self::robot_costs(&self.ore))? + state.new_resources(),
+            robot_counts: state.robot_counts.add_ore_robot(),
+        })
+    }
+
+    fn make_clay_robot(&self, state: &State) -> Option<State> {
+        Some(State {
+            remaining_minutes: state.remaining_minutes - 1,
+            resources: (state.resources - Self::robot_costs(&self.clay))? + state.new_resources(),
+            robot_counts: state.robot_counts.add_clay_robot(),
+        })
+    }
+
+    fn make_obsidian_robot(&self, state: &State) -> Option<State> {
+        Some(State {
+            remaining_minutes: state.remaining_minutes - 1,
+            resources: (state.resources - Self::robot_costs(&self.obsidian))?
+                + state.new_resources(),
+            robot_counts: state.robot_counts.add_obsidian_robot(),
+        })
+    }
+
+    fn make_geode_robot(&self, state: &State) -> Option<State> {
+        Some(State {
+            remaining_minutes: state.remaining_minutes - 1,
+            resources: (state.resources - Self::robot_costs(&self.geode))? + state.new_resources(),
+            robot_counts: state.robot_counts.add_geode_robot(),
+        })
+    }
+
+    const fn robot_costs<T>(robot: &Robot<T>) -> Resources {
+        Resources::from_robot(robot)
+    }
+}
+
+static INPUT_FILE: &str = "../inputs/day_19_test.input";
 
 fn main() -> anyhow::Result<()> {
     let blueprints: Vec<Blueprint> = fs::read_to_string(INPUT_FILE)
@@ -123,6 +338,10 @@ fn main() -> anyhow::Result<()> {
         .collect::<Result<Vec<Blueprint>, _>>()?;
 
     println!("The blueprints are {blueprints:?}");
+
+    let result: usize = blueprints.iter().map(Blueprint::quality_level).sum();
+
+    println!("The result is {result}.");
 
     Ok(())
 }
